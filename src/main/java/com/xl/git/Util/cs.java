651ba3784176d85e609df2d4d1edd6d599cbdc27
xllovely <3211969906@qq.com>
2019-03-08 11:04:44 +0800

    IdeaMaven first commit

--- src/main/java/com/xl/git/Util/cs.java
+++ src/main/java/com/xl/git/Util/cs.java
@@ -3,6 +3,6 @@ package com.xl.git.Util;
 public class cs {
     public static void main(String[] args) {
         System.out.print("1111111111111");
-        System.out.print("11111ssDDD");
+        System.out.print("11111ssDDD11111");
     }
 }
